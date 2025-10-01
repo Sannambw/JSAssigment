c5a19aa8858354653347e325b5c7cc0df0012ab5
Sannambw <sannambw@gmail.com>
2025-10-01 13:54:43 +0200

    Prova

--- js/app.js
+++ js/app.js
@@ -1,4 +1,50 @@
-console.log("hello world");
-console.log("Det här är en text grej");
-console.log("provar bara på hur man ska göra");
-console.log("kan man ändå prova det här");
+
+// Steg 1: Skapa variabler
+let numberOfBottles = 5;
+const line1 = "Green bottles hanging on the wall.";
+const line2 = "And if one green bottle, should accidentally fall, there will be";
+const line3   = "No green bottles hanging on the wall.";
+
+// Steg 2: Skriv ut i rätt ordning
+console.log(numberOfBottles); // här är hur många flaksor 10
+console.log(line1);// Green bottles hanging on the wall
+console.log(line2);//And if one green bottle, should accidentally fall, there will be
+console.log(4);
+console.log(line1);
+console.log(line2);
+console.log(3);
+console.log(line1);
+console.log(line1);
+console.log(2);
+console.log(line1);
+console.log(line2);
+console.log(1);
+console.log(line1);
+console.log(line2);
+console.log(0);
+console.log(line3)
+
+
+// Startvärde
+let silver = 20;
+console.log("Startvärde:", silver); // 20
+
+// Addition
+silver += 10; // samma som silver = silver + 10
+console.log("Efter addition (+= 10):", silver);
+console.log(silver+10);
+
+// Subtraktion
+silver -= 5; // samma som silver = silver - 5
+console.log("Efter subtraktion (-= 5):", silver); // 25
+
+// Multiplikation
+silver *= 4; // samma som silver = silver * 4
+console.log("Efter multiplikation (*= 4):", silver); // 100
+
+// Division
+silver /= 2; // samma som silver = silver / 2
+console.log("Efter division (/= 2):", silver); // 50
+
+
+
